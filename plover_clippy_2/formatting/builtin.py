e3e9cdbc874bf81ca06406ffabde78ceb509f481
josiah tan <josiah123t@gmail.com>
2022-02-21 02:37:28 +1100

    generalised color based sources

--- plover_clippy_2/formatting/builtin.py
+++ plover_clippy_2/formatting/builtin.py
@@ -12,15 +12,4 @@ class Formatting:
         self.sources = Sources([Org, Retro])
 
     def suggest(self, obj, clippy):
-        print("Hello world")
-        for source in self.sources.get():
-            if hasattr(source, "preprocess"):
-                source.preprocess(obj, clippy)
-            if hasattr(source, "addColor"):
-                source.addColor(obj, clippy)
-            if hasattr(source, "format"):
-                source.format(obj, clippy)
-            if hasattr(source, "extraColor"):
-                source.extraColor(obj, clippy)
-            if source.output:
-                clippy.actions.add(source.output)
+        self.sources.wrapColor(obj, clippy, "format")
